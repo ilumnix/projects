8f02d42d9acb6ce9086978f44293775f31112501
paulPortos <93170986+paulPortos@users.noreply.github.com>
2022-11-24 21:57:43 +0800

    Add files via upload

---
+++ UserAccountIdentification.java
@@ -0,0 +1,19 @@
+package basicProjects;
+
+public class UserAccountIdentification {
+	public static void main(String[]args) {
+		System.out.println("Welcome to Youtuve!");
+		System.out.println("Please select number!");
+		System.out.println("Login: 1");
+		System.out.println("Signup: 2");
+	}
+	public static void logIn() {
+		
+	}
+	public static void signUp() {
+		
+	}
+	public static void accountIdentificator() {
+		
+	}
+}
