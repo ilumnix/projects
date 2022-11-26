3092adc27ba63dc4b751e1c577eb64a4ae80f2dc
paulPortos <PaulVillacote.PGMR@gmail.com>
2022-11-26 10:28:19 +0800

    Update UserAccountIdentification.java

--- UserAccountIdentification.java
+++ UserAccountIdentification.java
@@ -6,8 +6,8 @@ public class UserAccountIdentification {
 
 	static String signUpUsername;
 	static String signUpPassword;
-	static String typedLoginPassword;
 	static String typedLoginUsername;
+	static String typedLoginPassword;
 	
 	public static void main(String[]args) {
 		System.out.println("Welcome to Youtuve!");
@@ -15,32 +15,49 @@ public class UserAccountIdentification {
 		System.out.println("Login: 1");
 		System.out.println("Signup: 2");
 		UserAccountIdentification.numberSelected = input.nextLine();
+		System.out.println("___________________");
 		transporter();
 	}
 	public static void logIn() {
 		System.out.println("Enter username: ");
+		UserAccountIdentification.typedLoginUsername = input.nextLine();
+		System.out.println("Enter password: ");
+		UserAccountIdentification.typedLoginPassword = input.nextLine();
+		System.out.println("___________________");
+		accountIdentificator();
 	}
 	public static void signUp() {
 		System.out.println("Enter username: ");
 		UserAccountIdentification.signUpUsername = input.nextLine();
 		System.out.println("Enter password: ");
 		UserAccountIdentification.signUpPassword = input.nextLine();
+		System.out.println("");
+		System.out.println("You have successfuly signed up!");
+		System.out.println("--------------------------------");
+		UserAccountIdentification.main(null);
 	}
 	public static void accountIdentificator() {
-		//*
+		if(signUpPassword.equals(typedLoginPassword) && signUpUsername.equals(typedLoginUsername)){
+				System.out.println("Welcome " + signUpUsername);
+		} else{
+			wrongInput();
+		}
 	}
 	public static void transporter() {
 		if(numberSelected.equals("1")) {
-			logIn();
+				logIn();
 		}else if(numberSelected.equals("2")){
-			signUp();
+				signUp();
 		} else {
-			System.out.println("Wrong input!");
-			System.out.println("Try Again!");
-			UserAccountIdentification.main(null);
+				System.out.println("Wrong input!");
+				System.out.println("Try Again!");
+				System.out.println("___________________");
+				UserAccountIdentification.main(null);
 		}
 	}
 	public static void wrongInput(){
 			System.out.println("Wrong password or username");
+			System.out.println("___________________");
+			logIn();
 	}
 }
